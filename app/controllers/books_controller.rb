ddf5e5a9f1b438d37a72a2597b16a30493f4633e
Callum Cleary <callumcleary1@hotmail.co.uk>
2022-08-29 16:38:24 +0100

    Controller, Views, Route

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,25 +1,53 @@
 class BooksController < ApplicationController
 
-def index
-@books = Book.all
-end
+  def index
+    @books = Book.all
+  end
 
-def show
-@book = Book.new
-end
+  def show
+    @user = User.find(params[:user_id])
+    @book = Book.find(params[:id])
+  end
 
-def new
-@book = Book.new
-end
+  def new
+    @user = User.find(params[:user_id])
+    @book = Book.new
+  end
 
-def destroy
-@book.destroy
-end
+  def create
+    @user = User.find(params[:user_id])
+    @book = Book.new(book_params)
+    @book.user = @user
+    if @book.save
+      redirect_to user_path(@user)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
-private
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
-def list_params
-  params.require(:book).permit(:title, :condition, :price)
-end
+  def edit
+    @user = User.find(params[:user_id])
+    @book = Book.find(params[:id])
+  end
 
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to user_path(@book.user), status: :see_other
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :condition, :price, :book_pic, :id)
+  end
 end
